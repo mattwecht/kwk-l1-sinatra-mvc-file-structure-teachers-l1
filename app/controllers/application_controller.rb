65c0443038ae04f125fb8bd682036e0cdcc7f05b
mattwecht <mattwecht@gmail.com>
2018-08-01 19:22:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
    answers = params.values #"value" in index
    @total= 0 #at the beginning of the quiz
    answers.each do |answer| #goes to the array of values
-     @total += answer.to_i #adds up numbers that user chose -- also convert to integer bc we're taking in numbers
+     @total += answer.to_i #adds up numbers that user chose -- also` convert to integer bc we're taking in numbers
    end
    puts @total
 
