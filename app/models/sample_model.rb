934a752ca8f069fd4a01ed73c6786acaae8c1e56
mattwecht <mattwecht@gmail.com>
2018-08-01 19:08:25 +0000

    automatically backed up by learn

---
+++ app/models/sample_model.rb
@@ -0,0 +1,12 @@
+def cool_generator(user_value)
+     if user_value < 15
+       return "not_cool" #refers to the page not_cool.erb
+    elsif user_value == 22
+       return "cool"
+    elsif user_value <= 24
+       return "not_cool"
+      
+      end
+    
+    
+end
